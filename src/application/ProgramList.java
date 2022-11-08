f3d37a63de5b813e9a04fc3f5b999fe2900eaf9c
Rafael De Luca <deluca1712@gmail.com>
2022-11-07 22:07:41 -0300

    Definicoes basicas de List

---
+++ src/application/ProgramList.java
@@ -0,0 +1,24 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProgramList {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\nLista é uma estrutura de dados\n"
+				+ "\tHomogênea, ordenada (acessar elementos por meio de posição).\n"
+				+ "\tInicia vazia e seus elementos são colocados sob demanda.\n"
+				+ "\tCada elemento ocupa um nó da lista.\n"
+				+ "\tExemplo: Lista encadeada\n"
+				+ "Vantagens\n"
+				+ "\tTamanho variável e facilidade para realizar Insert e Delete. (oposto de um vetor)\n"
+				+ "Desvantagens\n"
+				+ "\tAcesso sequencial aos elementos. (aposto do vetor)");
+		
+		List <String> lista = new ArrayList <> (); 
+		
+	}
+
+}
