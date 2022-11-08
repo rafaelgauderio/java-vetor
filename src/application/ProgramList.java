53bff3dccdd6bb9c5f3ccdfc80f143661416a912
Rafael De Luca <deluca1712@gmail.com>
2022-11-07 22:25:30 -0300

    Lista, metodos add, remove, removeIf, indexOf

--- src/application/ProgramList.java
+++ src/application/ProgramList.java
@@ -15,9 +15,44 @@ public class ProgramList {
 				+ "Vantagens\n"
 				+ "\tTamanho variável e facilidade para realizar Insert e Delete. (oposto de um vetor)\n"
 				+ "Desvantagens\n"
-				+ "\tAcesso sequencial aos elementos. (aposto do vetor)");
+				+ "\tAcesso sequencial aos elementos. Esse acesso é reduzido através da AraryList.(aposto do vetor)\n"
+				+ "\tList não aceita tipos primitivos. Tem que usar a classe Wrapper.\n\n");
 		
-		List <String> lista = new ArrayList <> (); 
+		List <String> lista = new ArrayList <String> ();
+		lista.add("Rafael de Luca");
+		lista.add("João da Silva");
+		lista.add("Claudia");
+		lista.add("Larissa Riquelme");
+		lista.add("Formiga Atômica");
+		
+		//inserindo elemento na posicao 3. Usa a sobrecarga do método add
+		lista.add(3, "Maria Madalena");
+		
+		System.out.println("Tamanho da lista: " + lista.size());
+		
+		for (String nickname : lista) {
+			System.out.println(nickname);
+		}
+		
+		//removendo o 2 item da lista
+		lista.remove(5);
+		
+		//removendo um objeto específico da lista
+		lista.remove("Claudia");
+		
+		//removendo usando um predicado. Todos que começam com a letra r
+		
+		lista.removeIf(x -> x.charAt(0) == 'R' || x.charAt(0) == 'r');
+		
+		System.out.println("\nImprimindo a lista após as remoções.");
+		for (String nickname : lista) {
+			System.out.println(nickname);
+		}
+		
+		System.out.println("\nIndex of Larissa: " + lista.indexOf("Larissa Riquelme"));
+		System.out.println("Index of Ulisses: " + lista.indexOf("Ulisses"));
+		
+		
 		
 	}
 
